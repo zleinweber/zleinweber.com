f86e4756e7bafad663c2bcfc4685c20c10513cde
Zach Leinweber <zleinweb@gmail.com>
2026-06-13 01:36:57 +0000

    Add the preact integration and create a interactive client island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   site: 'https://zleinweber.com',
+  integrations: [preact()],
 });
